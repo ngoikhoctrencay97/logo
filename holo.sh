f296c2096a1e25a99de7aeb14697f9f0c0e44361
Hoang Nguyen <31663074+ngoikhoctrencay97@users.noreply.github.com>
2023-11-16 16:32:59 +0700

    Update holo.sh

--- holo.sh
+++ holo.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-exists()
-{
+exists() {
   command -v "$1" >/dev/null 2>&1
 }
-if exists curl; then
-	echo ''
-else
+
+if ! exists curl; then
   sudo apt install curl -y < "/dev/null"
 fi
+
 curl -s https://raw.githubusercontent.com/ngoikhoctrencay97/logo/main/logo.sh | bash
 echo "==================================================="
 sleep 2
@@ -22,43 +21,44 @@ options=(
 select opt in "${options[@]}"
 do
 case $opt in
-
 "Install Node")
-echo "******************************************"
-echo -e "\e[1m\e[35m		Lets's begin\e[0m"
-echo "******************************************"
-echo -e "\e[1m\e[32m	Enter ALCHEMY_HTTP_ADDRESS:\e[0m"
-echo "******************************************"
-read ALCHEMY_HTTP_ADDRESS
-echo "******************************************"
-echo -e "\e[1m\e[35m	Enter CHAINSTACK_HTTP_ADDRESS:\e[0m"
-echo "******************************************"
-read CHAINSTACK_HTTP_ADDRESS
-echo "******************************************"
-echo export ALCHEMY_HTTP_ADDRESS=${ALCHEMY_HTTP_ADDRESS} >> $HOME/.bash_profile
-echo export CHAINSTACK_HTTP_ADDRESS=${CHAINSTACK_HTTP_ADDRESS} >> $HOME/.bash_profile
-source ~/.bash_profile
+  echo "******************************************"
+  echo -e "\e[1m\e[35m		Lets's begin\e[0m"
+  echo "******************************************"
+  echo -e "\e[1m\e[32m	Enter ALCHEMY_HTTP_ADDRESS:\e[0m"
+  echo "******************************************"
+  read ALCHEMY_HTTP_ADDRESS
+  echo "******************************************"
+  echo -e "\e[1m\e[35m	Enter CHAINSTACK_HTTP_ADDRESS:\e[0m"
+  echo "******************************************"
+  read CHAINSTACK_HTTP_ADDRESS
+  echo "******************************************"
+  echo export ALCHEMY_HTTP_ADDRESS=${ALCHEMY_HTTP_ADDRESS} >> $HOME/.bash_profile
+  echo export CHAINSTACK_HTTP_ADDRESS=${CHAINSTACK_HTTP_ADDRESS} >> $HOME/.bash_profile
+  source ~/.bash_profile
 
-echo -e "\e[1m\e[32m1. Updating packages and dependencies--> \e[0m" && sleep 1
-#UPDATE APT
-sudo apt update && sudo apt install --assume-yes build-essential git clang curl libssl-dev llvm libudev-dev git  make protobuf-compiler unzip
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
-source ~/.bashrc
-nvm install node
-nvm use node
-nvm alias default node
+  echo -e "\e[1m\e[32m1. Updating packages and dependencies--> \e[0m" && sleep 1
+  #UPDATE APT
+  sudo apt update && sudo apt install --assume-yes build-essential git clang curl libssl-dev llvm libudev-dev git  make protobuf-compiler unzip
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+  source ~/.bashrc
+  nvm install node
+  nvm use node
+  nvm alias default node
 
-echo -e "              \e[1m\e[32m2. Downloading and building binaries--> \e[0m" && sleep 1
+  echo -e "              \e[1m\e[32m2. Downloading and building binaries--> \e[0m" && sleep 1
 
-#INSTALL
-cd || return
-npm install -g @holographxyz/cli
-echo -e "              \e[1m\e[32m3. Create SystemD--> \e[0m" && sleep 1
-break
-;;
+  #INSTALL
+  cd || return
+  npm install -g @holographxyz/cli
+  echo -e "              \e[1m\e[32m3. Create SystemD--> \e[0m" && sleep 1
+  break
+  ;;
 "Delete Node")
-break
-;;
+  break
+  ;;
 "Exit")
+  exit
+  ;;
 esac
 done
